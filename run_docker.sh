b993ef170a846f0279f54facf24e34b5daa80f68
Ajay Kumar <kumar.327@iitj.ac.in>
2023-10-01 16:42:22 +0000

    fix

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-
+echo removing old containers
+docker stop $(docker ps -a -q)
 
 echo building docker containers
 docker-compose up --build -d
+
+echo Dash App status
+if [ $(curl -LI http://127.0.0.1:8000 -o /dev/null -w '%{http_code}\n' -s) == "200" ]; then echo Dash app running; fi
